a32784ee7fe5d8315772c05de078d0a0e993a990
dileepchebolu <54228708+dileepchebolu@users.noreply.github.com>
2020-01-13 16:47:23 +0530

    Update script-check.sh

--- script-check.sh
+++ script-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 echo "this is a small script to check the github webhook triggered jenkins project"
-echo "This is s acript named $SCRIPTNAME
+echo "This is s acript named $SCRIPTNAME"
 NUMBER=$(( $FIRSTNUMBER + $SECONDNUMBER ))
 echo "$NUMBER"
 echo " with this scrpt is completed "
